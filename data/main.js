37601640a4687659defb981d05554ed73f207332
MRBLVCKDEVTH <94088672+MRBLVCKDEVTH@users.noreply.github.com>
2022-03-12 13:47:37 -0700

    Inicio de animación.
    Se extendió el tiempo de inicio de animación de 300 milisegundos a 3 segundos.

--- data/main.js
+++ data/main.js
@@ -4,7 +4,7 @@ const typed = new Typed('.typed', {
 	],
 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
 	typeSpeed: 15, // Velocidad en mlisegundos para poner una letra,
-	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	startDelay: 3000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
 	backSpeed: 15, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
